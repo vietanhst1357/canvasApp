81d5413574b3d3b1ce81bd661acf134fabe938b5
= <=>
2021-07-26 14:13:55 +0700

    success

--- dda.js
+++ dda.js
@@ -1,19 +1,43 @@
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
 function dda() {
   var x0 = parseInt(document.form1.x1.value);
   var y0 = parseInt(document.form1.y1.value);
   var x1 = parseInt(document.form1.x2.value);
   var y1 = parseInt(document.form1.y2.value);
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
   ctx.beginPath();
   const dx = x1 - x0,
     dy = y1 - y0;
   var m = parseFloat(dy / dx);
   var x = x0,
     y = y0;
-  while(!(x>x1)){
-    setTimeout(ctx.fillRect(x,y,0,0), 500);
-    x=x+1;
-    y=y+m;
+  start0(x, y, m, x1);
+  start1(x, y, m, x1);
+}
+
+function start0(x, y, m, x1) {
+  if (x <= x1) {
+    setTimeout(function () {
+      ctx.moveTo(x, y);
+      x = x + 1;
+      y = y + m;
+      ctx.lineTo(x, y);
+      ctx.stroke();
+      start0(x, y, m, x1);
+    }, 5);
+  }
+}
+
+function start1(x, y, m, x1) {
+  if (x > x1) {
+    setTimeout(function () {
+      ctx.moveTo(x, y);
+      x = x - 1;
+      y = y - m;
+      ctx.lineTo(x, y);
+      ctx.stroke();
+      start1(x, y, m, x1);
+    }, 5);
   }
 }
