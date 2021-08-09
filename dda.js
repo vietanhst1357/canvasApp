cc7414a28806ec11290b4d8380a6824a2d92a050
= <=>
2021-08-09 16:57:39 +0700

    success

--- dda.js
+++ dda.js
@@ -1,7 +1,7 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-function clearCanvas(){
+function clearCanvas() {
   ctx.clearRect(0, 0, 1670, 1080);
 }
 
@@ -54,5 +54,50 @@ function start(x, y, m, x1, y1) {
       ctx.stroke();
       start(x, y, m, x1, y1);
     }, 5);
+  }
+}
+function drawPixel(x, y) {
+  ctx.fillRect(x, y, 2, 2);
+}
+function midPoint() {
+  var xc = parseInt(document.form1.xc.value);
+  var yc = parseInt(document.form1.yc.value);
+  var r = parseInt(document.form1.rc.value);
+  var color = document.form1.color.value;
+  ctx.fillStyle = color;
+  var p = 5 / 4 - r;
+  var x = 0;
+  var y = r;
+  draw8point(xc, yc, x, y);
+  runMP(xc, yc, x, y, p);
+}
+function draw8point(xc, yc, x, y) {
+  drawPixel(x + xc, y + yc);
+  drawPixel(-x + xc, y + yc);
+  drawPixel(x + xc, -y + yc);
+  drawPixel(-x + xc, -y + yc);
+  drawPixel(y + xc, x + yc);
+  drawPixel(-y + xc, x + yc);
+  drawPixel(y + xc, -x + yc);
+  drawPixel(-y + xc, -x + yc);
+}
+function runMP(xc, yc, x, y, p) {
+  if (x < y) {
+    if (p < 0) {
+      setTimeout(function () {
+        p = p + 2 * x + 3;
+        x = x + 1;
+        draw8point(xc, yc, x, y);
+        runMP(xc, yc, x, y, p);
+      }, 25);
+    } else {
+      setTimeout(function () {
+        p = p + 2 * (x - y) + 5;
+        y = y - 1;
+        x = x + 1;
+        draw8point(xc, yc, x, y);
+        runMP(xc, yc, x, y, p);
+      }, 25);
+    }
   }
 }
